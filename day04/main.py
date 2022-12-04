e1a42cd50b45937caa44a2106036845287b0118b
Max Marttinen <max.marttinen@gmail.com>
2022-12-04 11:04:14 +0100

    day04: solution

---
+++ day04/main.py
@@ -0,0 +1,42 @@
+import sys
+
+def fully_contains(a, b, c, d) -> bool:
+    if a <= c and b >= d:
+        return True
+    elif a >= c and b <= d:
+        return True
+    return False
+
+def part1(input: str) -> int:
+    overlap = 0
+    for elve_tasks in input.splitlines():
+        ab, cd = elve_tasks.split(',')
+        [a, b], [c, d] = ab.split('-'), cd.split('-')
+        a, b, c, d = int(a), int(b), int(c), int(d)
+
+        if fully_contains(a, b, c, d):
+            overlap += 1
+
+    return overlap
+
+def part2(input: str) -> int:
+    overlap = 0
+    for elve_tasks in input.splitlines():
+        ab, cd = elve_tasks.split(',')
+        [a, b], [c, d] = ab.split('-'), cd.split('-')
+        a, b, c, d = int(a), int(b), int(c), int(d)
+
+        if fully_contains(a, b, c, d):
+            overlap += 1
+        elif a <= c and c <= b <= d:
+            overlap += 1
+        elif b >= d and d >= a >= c:
+            overlap += 1
+
+    return overlap
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as f:
+        input = f.read()
+        print(part1(input))
+        print(part2(input))
