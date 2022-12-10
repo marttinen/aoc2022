a16f50709932e9c76c8d2fb3dcdc0f849850ce49
Max Marttinen <max.marttinen@gmail.com>
2022-12-10 12:19:18 +0100

    day10: part2, task failed successfully

--- day10/main.py
+++ day10/main.py
@@ -3,29 +3,53 @@ import os
 def part1(input: str) -> int:
     signals = []
     cycle = 0
-    reg_x = 1
+    regx = 1
 
-    def tick(cycle):
+    def tick():
+        nonlocal cycle
         cycle += 1
         if (cycle + 20) % 40 == 0:
-            signals.append(cycle * reg_x)
-            print(f'cycle {cycle}, X {reg_x}, signals {signals}')
-        return cycle
+            signals.append(cycle * regx)
 
     for command in input.splitlines():
         match command.split(' '):
             case ['noop']:
-                cycle = tick(cycle)
+                tick()
 
             case ['addx', x]:
-                cycle = tick(cycle)
-                cycle = tick(cycle)
-                reg_x += int(x)
+                tick()
+                tick()
+                regx += int(x)
 
     return sum(signals)
 
-def part2(input: str) -> int:
-    return 0
+def part2(input: str) -> str:
+    cycle = 0
+    regx = 1
+    crt = ''
+
+    def tick():
+        nonlocal cycle, crt
+        cycle += 1
+
+        if cycle % 40 == 0:
+            crt += '\n'
+        elif cycle % 40 in [regx-1, regx, regx+1]:
+            crt += '#'
+        else:
+            crt += '.'
+
+    for command in input.splitlines():
+        match command.split(' '):
+            case ['noop']:
+                tick()
+
+            case ['addx', x]:
+                tick()
+                regx += int(x)
+                tick()
+
+    return crt
 
 if __name__ == '__main__':
     dir = os.path.dirname(__file__)
